5a5bf2a3ed97c02d1c4dff01a5b1bb698addf2e6
Navrotskyj <navrotskyj@gmail.com>
2015-03-23 10:38:06 +0200

    enable Access-Control-Allow-Origin

--- src/worker.js
+++ src/worker.js
@@ -25,7 +25,7 @@ var enableCORS = function(req, res, next) {
     res.setHeader( 'X-Powered-By', 'Webitel CDR server' );
     res.header('Access-Control-Allow-Credentials', true);
     res.header('Access-Control-Allow-Origin', req.headers.origin);
-    //res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length');
 
