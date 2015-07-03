86fdf31ed3dbf40ad78769f3a98ab6cbd260e5e9
Navrotskyj <navrotskyj@gmail.com>
2015-07-03 16:59:59 +0300

    export elastic: add custom attribute

--- src/middleware/cdrToElastic/index.js
+++ src/middleware/cdrToElastic/index.js
@@ -11,6 +11,64 @@ var conf = require('../../config'),
 
 //var moment = require('moment');
 
+String.prototype.toHHMMSS = function () {
+    var sec_num = parseInt(this, 10); // don't forget the second param
+    var hours   = Math.floor(sec_num / 3600);
+    var minutes = Math.floor((sec_num - (hours * 3600)) / 60);
+    var seconds = sec_num - (hours * 3600) - (minutes * 60);
+
+    if (hours   < 10) {hours   = "0"+hours;}
+    if (minutes < 10) {minutes = "0"+minutes;}
+    if (seconds < 10) {seconds = "0"+seconds;}
+    var time    = hours + ':' + minutes + ':' + seconds;
+    return time;
+};
+
+function setCustomAttribute (record) {
+    try {
+        var callflow = record.callflow && record.callflow[0] || {
+                "caller_profile": {},
+                "extension": {},
+                "times": {}
+            };
+        //console.log(('' + record.variables.duration).toHHMMSS());
+        //console.log(('' + record.variables.billsec).toHHMMSS());
+
+        record["Call start time"] = record.variables.start_stamp; // +
+        record["Call answer time"] = record.variables.answer_stamp; // +
+        record["Call direction"] = record.variables.webitel_direction; // +
+        record["Hangup cause"] = record.variables.bridge_hangup_cause; // +
+        record["Q.850 Hangup Code"] = record.variables.hangup_cause_q850; // +
+        record["Call duration in seconds"] = record.variables.duration; // +
+        record["Call duration"] = ('' + record.variables.duration).toHHMMSS(); // +- todo
+        record["Connected call duration in seconds"] = record.variables.billsec; // +
+        record["Connected call duration"] = ('' + record.variables.billsec).toHHMMSS(); // +- todo
+        record["Call end time"] = record.variables.end_stamp; // +
+        record["Bridge time"] = record.variables.bridge_stamp; // +
+        record["Progress time"] = record.variables.progress_stamp; // +
+        record["Dialed User"] = record.variables.dialed_user; // +
+        record["Dialed Domain name"] = record.variables.dialed_domain; // +
+        record["Agent ID"] = record.variables.cc_agent; // +
+        record["Queue ID"] = record.variables.cc_queue; // +
+        record["Destination number"] = callflow.caller_profile.destination_number; // +
+        record["Call record in seconds"] = record.variables.record_seconds; // +
+        record["CallerID number"] = callflow.caller_profile.caller_id_number; // +
+        record["Domain name"] = record.variables.domain_name; // +
+        record["Bridged"] = record.variables.bridge_stamp ? true : false; // +
+        record["PDD"] = (callflow.times.progress_time > 0)
+            ? (callflow.times.progress_time + callflow.times.progress_media_time) - callflow.times.created_time
+            : 0; //
+        record["Ring Duration"] = (callflow.times.answered_time === 0)
+            ? callflow.times.hangup_time - callflow.times.created_time
+            : callflow.times.answered_time - callflow.times.created_time;
+
+    } catch (e) {
+        log.error(e);
+    } finally {
+        return record
+    };
+};
+
 function exportCollection(desc, mongoDb, callback) {
 
     var collection = mongoDb.collection(desc.name);
@@ -122,7 +180,7 @@ function exportCollection(desc, mongoDb, callback) {
                     index: indexName,
                     type: desc.type,
                     id: doc._id.toString(),
-                    body: doc
+                    body: setCustomAttribute(doc)
                 }, function (err) {
                     if (err) {
                         if (err['message'] && err['message'].indexOf('DocumentAlreadyExistsException') > -1) {
